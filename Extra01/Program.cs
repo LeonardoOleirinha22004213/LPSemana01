0b0dd17e25f23f11c7fc1c4dcb6dbebf4e1f11a1
Shorinoi <leonardo.oleirinha.work@gmail.com>
2026-02-19 16:47:28 +0000

    Rename Project

---
+++ Extra01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Extra01
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("\u00AE U00AE");
+        }
+    }
+}
